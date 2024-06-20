c778fc3a2d844f2fdf38e27f7ba96812d26e5cec
Jeroen De Dauw <jeroendedauw@gmail.com>
2024-06-20 18:33:41 +0200

    Add EvaluateApprovalState Action
    Fixes https://github.com/ProfessionalWiki/PageApprovals/issues/6

---
+++ src/Application/UseCases/EvaluateApprovalState.php
@@ -0,0 +1,32 @@
+<?php
+
+declare( strict_types = 1 );
+
+namespace ProfessionalWiki\PageApprovals\Application\UseCases;
+
+use ProfessionalWiki\PageApprovals\Application\ApprovalLog;
+use ProfessionalWiki\PageApprovals\Application\HtmlRepository;
+
+class EvaluateApprovalState {
+
+	public function __construct(
+		private readonly HtmlRepository $htmlRepository,
+		private readonly ApprovalLog $approvalLog
+	) {
+	}
+
+	public function evaluate( int $pageId, string $currentPageHtml ): void {
+		if ( $currentPageHtml !== $this->getApprovedHtmlForPage( $pageId ) ) {
+			$this->unapprovePage( $pageId );
+		}
+	}
+
+	private function getApprovedHtmlForPage( int $pageId ): ?string {
+		return $this->htmlRepository->getApprovedHtml( $pageId );
+	}
+
+	private function unapprovePage( int $pageId ): void {
+		$this->approvalLog->unapprovePage( pageId: $pageId, userId: null );
+	}
+
+}
---
+++ src/EntryPoints/PageApprovalsHooks.php
@@ -0,0 +1,20 @@
+<?php
+
+declare( strict_types = 1 );
+
+namespace ProfessionalWiki\PageApprovals\EntryPoints;
+
+use OutputPage;
+use ParserOutput;
+use ProfessionalWiki\PageApprovals\PageApprovals;
+
+class PageApprovalsHooks {
+
+	public static function onOutputPageParserOutput( OutputPage $out, ParserOutput $parserOutput ): void {
+		PageApprovals::getInstance()->newEvaluateApprovalStateAction()->evaluate(
+			pageId: $out->getWikiPage()->getId(),
+			currentPageHtml: $parserOutput->getRawText(),
+		);
+	}
+
+}
