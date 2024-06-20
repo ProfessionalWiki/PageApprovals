0263d84f995061dd6c8ba279164bf8e236233b11
Jeroen De Dauw <jeroendedauw@gmail.com>
2024-06-20 21:54:19 +0200

    Add InMemoryApprovalLog and InMemoryHtmlRepository

--- src/Application/ApprovalLog.php
+++ src/Application/ApprovalLog.php
@@ -6,10 +6,10 @@ namespace ProfessionalWiki\PageApprovals\Application;
 
 interface ApprovalLog {
 
-	public function getApprovalState( int $pageId ): ApprovalState;
+	public function getApprovalState( int $pageId ): ?ApprovalState;
 
-	public function markPageAsApproved( int $pageId, int $userId ): void;
+	public function unapprovePage( int $pageId, ?int $userId ): void;
 
-	public function markPageAsUnapproved( int $pageId, ?int $userId ): void;
+	public function approvePage( int $pageId, int $userId ): void;
 
 }
--- src/Application/HtmlRepository.php
+++ src/Application/HtmlRepository.php
@@ -6,7 +6,7 @@ namespace ProfessionalWiki\PageApprovals\Application;
 
 interface HtmlRepository {
 
-	public function getApprovedHtml( int $pageId ): string;
+	public function getApprovedHtml( int $pageId ): ?string;
 
 	public function saveApprovedHtml( int $pageId, string $html ): void;
 
