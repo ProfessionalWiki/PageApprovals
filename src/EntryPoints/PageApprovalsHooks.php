81af0c20c4b9233dcaa818b7bfdb98205de35fad
waximabbax <waximabbax@gmail.com>
2024-06-22 08:22:02 +0500

    Translation Typo Correction

--- src/EntryPoints/PageApprovalsHooks.php
+++ src/EntryPoints/PageApprovalsHooks.php
@@ -4,62 +4,67 @@ declare( strict_types = 1 );
 
 namespace ProfessionalWiki\PageApprovals\EntryPoints;
 
-use MediaWiki\MediaWikiServices;
+use DatabaseUpdater;
 use OutputPage;
 use ParserOutput;
 use ProfessionalWiki\PageApprovals\PageApprovals;
-use Title;
-use Wikimedia\Rdbms\DBError;
 
 class PageApprovalsHooks {
 
 	public static function onOutputPageParserOutput( OutputPage $out, ParserOutput $parserOutput ): void {
-		PageApprovals::getInstance()->newEvaluateApprovalStateAction()->evaluate(
-			pageId: $out->getWikiPage()->getId(),
-			currentPageHtml: $parserOutput->getRawText(),
+		if ( $out->isArticle() ) {
+			PageApprovals::getInstance()->newEvaluateApprovalStateAction()->evaluate(
+				pageId: $out->getWikiPage()->getId(),
+				currentPageHtml: $parserOutput->getRawText(),
+			);
+		}
+	}
+
+	public static function onLoadExtensionSchemaUpdates( DatabaseUpdater $updater ): void {
+		$updater->addExtensionTable(
+			'approval_log',
+			__DIR__ . '/../../sql/PageApprovals.sql'
 		);
 	}
 
 	public static function onOutputPageBeforeHTML( OutputPage $out ): void {
-		$user = $out->getUser();
-		$title = $out->getTitle();
+		// TODO: Build logic for all hardcoded Booleans
+		$isUserAnApprover = true;
+		$isPageApproved = false;
+		// @phpstan-ignore-next-line
+		$isPageApprovable = $out->getTitle()->isContentPage() && !$out->getTitle()->isSpecialPage();
+		$isAnApproverForPageCategory = true;
 
-		if ( !$title instanceof Title || !$user->isRegistered() ) {
-			return;
+		if ( $isPageApprovable ) {
+			self::showApprovalStatus( $out, $isPageApproved );
 		}
 
-		$permissionManager = MediaWikiServices::getInstance()->getPermissionManager();
-		$revisionStore = MediaWikiServices::getInstance()->getRevisionStore();
-
-		try {
-			$firstRevision = $revisionStore->getFirstRevision( $title );
-			$firstUserId = $firstRevision ? $firstRevision->getUser( 0 ) : null;
-		} catch ( DBError $e ) {
-			wfLogWarning( "Database error fetching first revision: " . $e->getMessage() );
-			return;
-		}
-
-		$canApprove = $title->isContentPage() && !$title->isSpecialPage() && $permissionManager->userCan(
-				'edit',
-				$user,
-				$title
-			) && $user->getId() !== $firstUserId; // TODO: Placeholder for actual Logic
-
-		$isApproved = false; // TODO: build Logic
-
 		// @phpstan-ignore-next-line
-		$messageKey = $isApproved ? "pageapprovals-status-approved" : "pageapprovals-status-not-approved";
-		$message = $out->msg( $messageKey )->text();
-		$out->addHTML( "<div class='page-approval-status'>{$message}</div>" );
+		$canApprove = $isUserAnApprover && $isPageApprovable && $isAnApproverForPageCategory;
 
-		// @phpstan-ignore-next-line
-		if ( $canApprove && !$isApproved ) {
-			$approveButtonText = $out->msg( 'pageapprovals-approve-button' )->text();
-			$buttonHtml = "<button id='approveButton'>{$approveButtonText}</button>";
-			$out->addHTML( $buttonHtml );
+		if ( $canApprove ) {
+			self::showApprovalButton( $out, $isPageApproved );
 		}
 
 		$out->addModules( 'ext.pageApprovals.resources' );
+	}
+
+	private static function showApprovalStatus( OutputPage $out, bool $isPageApproved ): void {
+		$messageKey = $isPageApproved ? "pageapprovals-status-approved" : "pageapprovals-status-not-approved";
+		$message = $out->msg( $messageKey )->text();
+		$out->addHTML( "<div class='page-approval-status'>{$message}</div>" );
+	}
+
+	private static function showApprovalButton( OutputPage $out, bool $isPageApproved ): void {
+		if ( $isPageApproved ) {
+			$unapproveButtonText = $out->msg( 'pageapprovals-unapprove-button' )->text();
+			$unapproveButtonHtml = "<button id='unapproveButton'>{$unapproveButtonText}</button>";
+			$out->addHTML( $unapproveButtonHtml );
+		} else {
+			$approveButtonText = $out->msg( 'pageapprovals-approve-button' )->text();
+			$approveButtonHtml = "<button id='approveButton'>{$approveButtonText}</button>";
+			$out->addHTML( $approveButtonHtml );
+		}
 	}
 
 }
