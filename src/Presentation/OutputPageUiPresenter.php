9dadeb00e469e83438e077f0b8a851a7872ee874
Jeroen De Dauw <jeroendedauw@gmail.com>
2024-06-28 22:08:10 +0200

    Refactor to ApprovalUiQuery UC with Presenter
    No new logic, tests still missing, but these can now be added as next step
    
    Manually verified

---
+++ src/Presentation/OutputPageUiPresenter.php
@@ -0,0 +1,39 @@
+<?php
+
+declare( strict_types = 1 );
+
+namespace ProfessionalWiki\PageApprovals\Presentation;
+
+use OutputPage;
+use ProfessionalWiki\PageApprovals\Application\UseCases\ApprovalUiQuery\UiArguments;
+use ProfessionalWiki\PageApprovals\PageApprovals;
+
+class OutputPageUiPresenter {
+
+	public function __construct(
+		private readonly OutputPage $out
+	) {
+	}
+
+	public function presentUi( UiArguments $arguments ): void {
+		// TODO: use $arguments->approvalTimestamp and $arguments->approverId
+
+		$this->out->addHTML(
+			PageApprovals::getInstance()->getTemplateParser()->processTemplate(
+				'PageApprovalStatus',
+				[
+					'isPageApproved' => $arguments->pageIsApproved,
+					'canApprove' => $arguments->userIsApprover,
+					'approveButtonText' => $this->out->msg( 'pageapprovals-approve-button' )->text(),
+					'unapproveButtonText' => $this->out->msg( 'pageapprovals-unapprove-button' )->text(),
+					'approvalStatusMessage' => $this->out->msg(
+						$arguments->pageIsApproved ? 'pageapprovals-status-approved' : 'pageapprovals-status-not-approved'
+					)->text()
+				]
+			)
+		);
+
+		$this->out->addModules( 'ext.pageApprovals.resources' );
+	}
+
+}
