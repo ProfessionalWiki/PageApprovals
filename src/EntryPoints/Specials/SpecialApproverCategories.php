c70437d5d1d95e0a6de197c4a0c8329e57b4cb8d
Jeroen De Dauw <jeroendedauw@gmail.com>
2024-06-29 23:47:19 +0200

    Use correct property type

--- src/EntryPoints/Specials/SpecialApproverCategories.php
+++ src/EntryPoints/Specials/SpecialApproverCategories.php
@@ -3,8 +3,9 @@
 namespace ProfessionalWiki\PageApprovals\EntryPoints\Specials;
 
 use MediaWiki\MediaWikiServices;
-use ProfessionalWiki\PageApprovals\Adapters\DatabaseApproverRepository;
+use ProfessionalWiki\PageApprovals\Application\ApproverRepository;
 use ProfessionalWiki\PageApprovals\Application\UseCases\GetApproversWithCategories;
+use ProfessionalWiki\PageApprovals\PageApprovals;
 use SpecialPage;
 use PermissionsError;
 use LightnCandy\LightnCandy;
@@ -12,15 +13,11 @@ use WebRequest;
 
 class SpecialApproverCategories extends SpecialPage {
 
-	/**
-	 * @var DatabaseApproverRepository
-	 */
-	private $databaseApproverRepository;
+	private ApproverRepository $approverRepository;
 
 	public function __construct() {
 		parent::__construct( 'ApproverCategories' );
-		$db = MediaWikiServices::getInstance()->getDBLoadBalancer()->getConnection( DB_PRIMARY );
-		$this->databaseApproverRepository = new DatabaseApproverRepository( $db );
+		$this->approverRepository = PageApprovals::getInstance()->getApproverRepository();
 	}
 
 	public function isListed(): bool {
@@ -32,7 +29,7 @@ class SpecialApproverCategories extends SpecialPage {
 			throw new PermissionsError( 'sysop' );
 		}
 
-		$approversWithCategories = new GetApproversWithCategories( $this->databaseApproverRepository );
+		$approversWithCategories = new GetApproversWithCategories( $this->approverRepository );
 		$approversCategories = $approversWithCategories->getApproversWithCategories();
 
 		$request = $this->getRequest();
@@ -90,7 +87,7 @@ class SpecialApproverCategories extends SpecialPage {
 			default:
 				return;
 		}
-		$this->databaseApproverRepository->setApproverCategories( $userId, $currentCategories );
+		$this->approverRepository->setApproverCategories( $userId, $currentCategories );
 	}
 
 	private function renderHtml( array $approversCategories ): void {
