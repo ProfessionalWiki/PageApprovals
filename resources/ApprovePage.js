87c0d95c3e66e06a635c75589e898c6c7ce61223
Morne Alberts <morne@malberts.net>
2024-07-04 10:05:20 +0200

    Fix ESLint issues

--- resources/ApprovePage.js
+++ resources/ApprovePage.js
@@ -1,3 +1,40 @@
+const convertTimestamps = ( timestamp = null ) => {
+	try {
+		const moment = require( 'moment' );
+		$( '.convert-timestamp' ).each( function () {
+			const $element = $( this );
+			const unixTimestamp = parseInt( timestamp || $element.attr( 'data-timestamp' ) );
+			const momentDate = moment.unix( unixTimestamp ).utc();
+			$element.text( momentDate.fromNow() );
+		} );
+	} catch ( error ) {
+		console.error( 'Error converting timestamps:', error );
+	}
+};
+
+const handleApprovalResponse = ( approve, data ) => {
+	const elements = [
+		'#unapproveButton',
+		'#approveButton',
+		'.approved-status-text',
+		'.tooltip',
+		'.approver-details',
+		'approval-dropdown'
+	];
+	const $badgeElement = $( '.approval-status-badge' );
+
+	$badgeElement.toggleClass( 'approved-badge' );
+	$badgeElement.toggleClass( 'unapproved-badge' );
+
+	elements.forEach( ( selector ) => $( selector ).toggleClass( 'display-none' ) );
+
+	if ( approve ) {
+		const { approver, approvalTimestamp } = data;
+		$( '.approver-name' ).text( approver );
+		convertTimestamps( approvalTimestamp );
+	}
+};
+
 const sendApprovalRequest = ( approve ) => {
 	const restClient = new mw.Rest();
 	const revisionId = mw.config.get( 'wgRevisionId' );
@@ -13,56 +50,19 @@ const sendApprovalRequest = ( approve ) => {
 		} );
 };
 
-const handleApprovalResponse = ( approve, data ) => {
-	const elements = [
-		'#unapproveButton',
-		'#approveButton',
-		'.approved-status-text',
-		'.tooltip',
-		'.approver-details',
-		'approval-dropdown'
-	];
-	const badgeElement = $( '.approval-status-badge' );
-
-	badgeElement.toggleClass( 'approved-badge' );
-	badgeElement.toggleClass( 'unapproved-badge' );
-
-	elements.forEach( selector => $( selector ).toggleClass( 'display-none' ) );
-
-	if( approve ) {
-		const { approver, approvalTimestamp } = data;
-		$( '.approver-name' ).text( approver );
-		convertTimestamps( approvalTimestamp );
-	}
-};
-
-$( '#approveButton, #unapproveButton' ).click( function() {
+$( '#approveButton, #unapproveButton' ).on( 'click', function () {
 	sendApprovalRequest( $( this ).is( '#approveButton' ) );
 } );
 
-$( '.approval-status-badge:not(.approval-dropdown)' ).click( e => {
+$( '.approval-status-badge:not(.approval-dropdown)' ).on( 'click', ( e ) => {
 	e.stopPropagation();
 	$( '.approval-dropdown' ).toggleClass( 'display-none' );
 } );
 
-$( document ).click( e => {
-	if( !$( e.target ).closest( '.approval-dropdown, .approval-status-badge' ).length ) {
+$( document ).on( 'click', ( e ) => {
+	if ( !$( e.target ).closest( '.approval-dropdown, .approval-status-badge' ).length ) {
 		$( '.approval-dropdown' ).addClass( 'display-none' );
 	}
 } );
 
-const convertTimestamps = ( timestamp = null ) => {
-	try {
-		const moment = require( 'moment' );
-		$( '.convert-timestamp' ).each( function() {
-			const $element = $( this );
-			const unixTimestamp = parseInt( timestamp || $element.attr( 'data-timestamp' ) );
-			const momentDate = moment.unix( unixTimestamp ).utc();
-			$element.text( momentDate.fromNow() );
-		} );
-	} catch( error ) {
-		console.error( 'Error converting timestamps:', error );
-	}
-};
-
-$( document ).ready( convertTimestamps() );
+$( convertTimestamps() );
