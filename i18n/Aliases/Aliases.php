50eb72e1cfea2e47e550eab1a0c1eade19667dda
Karsten Hoffmeyer <mediawiki@kghoffmeyer.de>
2024-07-13 18:41:03 +0200

    Update Aliases.php
    * Added translations for de
    * Added language comment to adhere to common MW practice
    * Removed qqq since it is not supported here

--- i18n/Aliases/Aliases.php
+++ i18n/Aliases/Aliases.php
@@ -1,12 +1,15 @@
 <?php
+
 $specialPageAliases = [];
 
+/** English (English) */
 $specialPageAliases['en'] = [
 	'ManageApprovers' => [ 'ManageApprovers' ],
 	'PendingApprovals' => [ 'PendingApprovals' ],
 ];
 
-$specialPageAliases['qqq'] = [
-	'ManageApprovers' => [ 'ManageApprovers' ],
-	'PendingApprovals' => [ 'PendingApprovals' ],
+/** German (Deutsch) */
+$specialPageAliases['de'] = [
+	'ManageApprovers' => [ 'Genehmiger_verwalten' ],
+	'PendingApprovals' => [ 'Ausstehende_Genehmigungen' ],
 ];
